0088b3520e8ebd866ed19681a8f5c62a215f5137
Ahmed Sultan <ahmeddsultann26@gmail.com>
2025-06-01 19:08:13 -0400

    Setup

---
+++ Basics/addition.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sultaniko on 6/1/25.
+//
+
+#include "addition.h"
---
+++ Basics/addition.h
@@ -0,0 +1,16 @@
+//
+// Created by sultaniko on 6/1/25.
+//
+
+#ifndef ADDITION_H
+#define ADDITION_H
+
+
+
+class addition {
+
+};
+
+
+
+#endif //ADDITION_H
